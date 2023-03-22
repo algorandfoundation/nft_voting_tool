b7332ce6dab6d66c98d8c841545c8a7e1c6bf0fe
Stafford Williams <stafford.williams@gmail.com>
2023-03-22 17:54:33 +1100

    variable name

--- src/dapp/src/features/vote/VoteResults.tsx
+++ src/dapp/src/features/vote/VoteResults.tsx
@@ -10,14 +10,14 @@ const values = [200, 561, 302, 482];
 export const VoteResults = ({ round }: VoteResultsProps) => {
   const max = Math.max(...values);
   const sum = values.reduce((a, b) => a + b, 0);
-  const percent = values.map((v) => (v / max) * 150);
+  const pixelWidth = values.map((v) => (v / max) * 150);
   return (
     <>
       <div className="grid grid-cols-3 w-80 gap-2">
         {round.answers.map((option, ix) => (
           <>
             <div className="col-span-2 h-10 flex items-center">
-              <div className="bg-algorand-orange-coral h-10 rounded-tr-xl rounded-br-xl" style={{ flexBasis: `${percent[ix]}px` }}></div>
+              <div className="bg-algorand-orange-coral h-10 rounded-tr-xl rounded-br-xl" style={{ flexBasis: `${pixelWidth[ix]}px` }}></div>
               <div className="p-2 pr-6">{values[ix]}</div>
             </div>
             <div className="flex  items-center">{option}</div>
