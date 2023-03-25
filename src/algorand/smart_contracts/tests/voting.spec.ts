defff860b7b32fd347ee91df40b584e26d5b2422
Rob Moore <rob.moore@makerx.com.au>
2023-03-25 23:57:14 +0800

    Completed initial test coverage

--- src/algorand/smart_contracts/tests/voting.spec.ts
+++ src/algorand/smart_contracts/tests/voting.spec.ts
@@ -8,7 +8,7 @@ import { AppSpec } from '@algorandfoundation/algokit-utils/types/appspec'
 import * as ed from '@noble/ed25519'
 import * as uuid from 'uuid'
 import invariant from 'tiny-invariant'
-import { encodeAnswerIdBoxRefs, encodeAnswerIds } from '../question-encoding'
+import { encodeAnswerId, encodeAnswerIdBoxRef, encodeAnswerIdBoxRefs, encodeAnswerIds } from '../question-encoding'
 import { ABIUintType } from 'algosdk'
 import algosdk from 'algosdk'
 
@@ -38,12 +38,14 @@ describe('voting', () => {
     const round = await algod.block(lastRound).do()
     const currentTime = Number(round.block.ts)
 
+    const voteFee = algokit.microAlgos(1_000 + 3 /* opup - 700 x 3 to get 2000 */ * 1_000)
+
     const privateKey = Buffer.from(ed.utils.randomPrivateKey())
     const publicKey = await ed.getPublicKey(privateKey)
 
     cid = cid ?? 'CID'
     start = start ?? currentTime
-    end = end ?? currentTime + 100
+    end = end ?? currentTime + 1000
     quorum = quorum ?? Math.ceil(Math.random() * 1000)
     questionIds = questionIds ?? [uuid.v4(), uuid.v4(), uuid.v4(), uuid.v4()]
     const questions = {
@@ -122,6 +124,18 @@ describe('voting', () => {
       }
     }
 
+    const vote = async (voter: { account: Account; signature: Uint8Array }, questionIndex: number) => {
+      return await appClient.call({
+        method: 'vote',
+        methodArgs: {
+          args: [voter.signature, questions.encoded[questionIndex]],
+          boxes: [questions.boxRefs[questionIndex]],
+        },
+        sendParams: { fee: voteFee },
+        sender: voter.account,
+      })
+    }
+
     return {
       algod,
       testAccount,
@@ -137,7 +151,8 @@ describe('voting', () => {
       questions,
       bootstrap,
       getVoter,
-      voteFee: algokit.microAlgos(1_000 + 3 /* opup - 700 x 3 to get 2000 */ * 1_000),
+      voteFee,
+      vote,
     }
   }
 
@@ -175,8 +190,32 @@ describe('voting', () => {
     expect(boxValues.map((v) => v.value)).toEqual([0n, 0n, 0n, 0n])
   })
 
+  test('double bootstrap', async () => {
+    const { bootstrap } = await setupApp()
+    await bootstrap()
+
+    try {
+      await bootstrap()
+      invariant(false)
+    } catch (e: any) {
+      expect(e.stack).toMatchInlineSnapshot(`
+        "assert
+        bytec_1 // "is_bootstrapped"
+        app_global_get
+        !
+        // Already bootstrapped
+        assert <--- Error
+        bytec_1 // "is_bootstrapped"
+        intc_1 // 1
+        app_global_put
+        pushint 2500 // 2500"
+      `)
+    }
+  })
+
   test('get_preconditions', async () => {
-    const { appClient, getVoter, voteFee, currentTime, start, end } = await setupApp()
+    const { appClient, getVoter, voteFee, currentTime, bootstrap } = await setupApp()
+    await bootstrap()
     const voter = await getVoter()
 
     const result = await appClient.call({
@@ -195,29 +234,168 @@ describe('voting', () => {
     expect(Number(time)).toBeGreaterThan(currentTime)
   })
 
-  test('successful vote', async () => {
-    const { appClient, questions, getVoter, voteFee } = await setupApp()
-    const voter = await getVoter()
+  describe('vote', () => {
+    test('successful', async () => {
+      const { appClient, questions, getVoter, vote, bootstrap } = await setupApp()
+      await bootstrap()
+      const voter = await getVoter()
 
-    await appClient.call({
-      method: 'vote',
-      methodArgs: {
-        args: [voter.signature, questions.encoded[0]],
-        boxes: [questions.boxRefs[0]],
-      },
-      sendParams: { fee: voteFee },
-      sender: voter.account,
+      await vote(voter, 0)
+
+      const boxValue = await appClient.getBoxValuesAsABIType(
+        new ABIUintType(64),
+        (b) => b.nameBase64 === Buffer.from(questions.boxRefs[0].name).toString('base64'),
+      )
+      expect(boxValue[0].value).toBe(1n)
+      const otherBoxValues = await appClient.getBoxValuesAsABIType(
+        new ABIUintType(64),
+        (b) => b.nameBase64 !== Buffer.from(questions.boxRefs[0].name).toString('base64'),
+      )
+      expect(otherBoxValues.map((v) => v.value)).toEqual([0n, 0n, 0n])
     })
 
-    const boxValue = await appClient.getBoxValuesAsABIType(
-      new ABIUintType(64),
-      (b) => b.name === questions.boxRefs[0].name,
-    )
-    expect(boxValue[0].value).toBe(1n)
-    const otherBoxValues = await appClient.getBoxValuesAsABIType(
-      new ABIUintType(64),
-      (b) => b.name !== questions.boxRefs[0].name,
-    )
-    expect(otherBoxValues.map((v) => v.value)).toEqual([0n, 0n, 0n])
+    /* todo: test('double voting', async () => {
+    const { getVoter, bootstrap, vote } = await setupApp()
+    await bootstrap()
+    const voter = await getVoter()
+
+    await vote(voter, 0)
+    try {
+      await vote(voter, 1)
+      invariant(false)
+    } catch (e: any) {
+      expect(e.stack).toMatchInlineSnapshot()
+    }
+  })*/
+
+    test('not bootstrapped', async () => {
+      const { getVoter, vote } = await setupApp()
+      const voter = await getVoter()
+
+      try {
+        await vote(voter, 0)
+        invariant(false)
+      } catch (e: any) {
+        expect(e.stack).toMatchInlineSnapshot(`
+                  "callsub allowedtovote_3
+                  // Allowed to vote
+                  assert
+                  callsub votingopen_4
+                  // Voting open
+                  assert <--- Error
+                  callsub alreadyvoted_5
+                  !
+                  // Hasn't already voted
+                  assert"
+              `)
+      }
+    })
+
+    test('invalid signature', async () => {
+      const { getVoter, vote, bootstrap } = await setupApp()
+      await bootstrap()
+      const voter = await getVoter()
+      const voter2 = await getVoter()
+
+      try {
+        await vote({ account: voter.account, signature: voter2.signature }, 0)
+        invariant(false)
+      } catch (e: any) {
+        expect(e.stack).toMatchInlineSnapshot(`
+                  "proto 2 0
+                  frame_dig -2
+                  extract 2 0
+                  callsub allowedtovote_3
+                  // Allowed to vote
+                  assert <--- Error
+                  callsub votingopen_4
+                  // Voting open
+                  assert
+                  callsub alreadyvoted_5"
+              `)
+      }
+    })
+
+    test('early vote', async () => {
+      const start = +new Date('2099-12-31T11:59:59Z')
+      const { getVoter, vote, bootstrap } = await setupApp({ start, end: start + 1 })
+      await bootstrap()
+      const voter = await getVoter()
+
+      try {
+        await vote(voter, 0)
+        invariant(false)
+      } catch (e: any) {
+        expect(e.stack).toMatchInlineSnapshot(`
+                  "callsub allowedtovote_3
+                  // Allowed to vote
+                  assert
+                  callsub votingopen_4
+                  // Voting open
+                  assert <--- Error
+                  callsub alreadyvoted_5
+                  !
+                  // Hasn't already voted
+                  assert"
+              `)
+      }
+    })
+
+    test('late vote', async () => {
+      const end = 2
+      const { getVoter, vote, bootstrap } = await setupApp({ start: end - 1, end })
+      await bootstrap()
+      const voter = await getVoter()
+
+      try {
+        await vote(voter, 0)
+        invariant(false)
+      } catch (e: any) {
+        expect(e.stack).toMatchInlineSnapshot(`
+                  "callsub allowedtovote_3
+                  // Allowed to vote
+                  assert
+                  callsub votingopen_4
+                  // Voting open
+                  assert <--- Error
+                  callsub alreadyvoted_5
+                  !
+                  // Hasn't already voted
+                  assert"
+              `)
+      }
+    })
+
+    test('invalid question', async () => {
+      const { appClient, getVoter, bootstrap, voteFee } = await setupApp()
+      await bootstrap()
+      const voter = await getVoter()
+
+      try {
+        await appClient.call({
+          method: 'vote',
+          methodArgs: {
+            args: [voter.signature, encodeAnswerId('a')],
+            boxes: [encodeAnswerIdBoxRef('a')],
+          },
+          sendParams: { fee: voteFee },
+          sender: voter.account,
+        })
+        invariant(false)
+      } catch (e: any) {
+        expect(e.stack).toMatchInlineSnapshot(`
+          "box_len
+          store 24
+          store 23
+          load 24
+          // Answer ID valid
+          assert <--- Error
+          bytec_0 // "V_"
+          frame_dig -1
+          concat
+          box_get"
+        `)
+      }
+    })
   })
 })
