fca7d321c03552a9363fd2ee3823bd8ed4fe3747
Vatsalya Goel <vatsalyagoel@gmail.com>
2023-03-27 17:15:36 +0400

    PR Feedback

--- src/voting-metadata-api/src/controllers/ipfsController.ts
+++ src/voting-metadata-api/src/controllers/ipfsController.ts
@@ -1,27 +1,29 @@
-import { Readable } from "stream";
-import { Controller, Get, Path, Post, Route, UploadedFile } from "tsoa";
-import { inject, injectable } from "tsyringe";
-import { IIpfsService } from "../services/ipfsService";
+import { Readable } from 'stream'
+import { Controller, Get, Path, Post, Route, UploadedFile } from 'tsoa'
+import { inject, injectable } from 'tsyringe'
+import { IIpfsService } from '../services/ipfsService'
 
 @injectable()
 @Route('ipfs')
 export class IpfsController extends Controller {
-    private ipfsService: IIpfsService;
+  private ipfsService: IIpfsService
 
-    constructor(@inject("IIpfsService") ipfsService: IIpfsService) {
-        super();
-        this.ipfsService = ipfsService;
-    }
+  constructor(@inject('IIpfsService') ipfsService: IIpfsService) {
+    super()
+    this.ipfsService = ipfsService
+  }
 
-    @Get("{cid}")
-    public async getWalletSnapshotFile(@Path() cid: string): Promise<Readable> {
-        const [buffer, mimeType] = await this.ipfsService.getBuffer(cid);
-        this.setHeader("Content-Type", mimeType);
-        return Readable.from(buffer);
-    }
+  @Get('{cid}')
+  public async get(@Path() cid: string): Promise<Readable> {
+    const [buffer, mimeType] = await this.ipfsService.getBuffer(cid)
+    this.setHeader('Content-Type', mimeType)
+    return Readable.from(buffer)
+  }
 
-    @Post()
-    public async postWalletSnapshot(@UploadedFile() snapshot: Express.Multer.File): Promise<{ cid: string }> {
-        return this.ipfsService.putBuffer(snapshot.buffer, snapshot.mimetype);
-    }
+  @Post()
+  public async post(
+    @UploadedFile() snapshot: Express.Multer.File
+  ): Promise<{ cid: string }> {
+    return this.ipfsService.putBuffer(snapshot.buffer, snapshot.mimetype)
+  }
 }
