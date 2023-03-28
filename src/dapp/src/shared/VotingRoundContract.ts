d714299845849651cc5d8c9c94ab2aeb6bc9a5ec
Richard Watts-Seale <rwattsseale@gmail.com>
2023-03-28 12:11:27 +1100

    Lint fix

--- src/dapp/src/shared/VotingRoundContract.ts
+++ src/dapp/src/shared/VotingRoundContract.ts
@@ -1,26 +1,26 @@
-import * as algokit from "@algorandfoundation/algokit-utils";
-import { AppReference } from "@algorandfoundation/algokit-utils/types/app";
-import { TransactionSigner } from "algosdk";
-import * as appSpec from "../../../algorand/smart_contracts/artifacts/VotingRoundApp/application.json";
-import { encodeAnswerId, encodeAnswerIdBoxRef, encodeAnswerIdBoxRefs, encodeAnswerIds } from "./question-encoding";
+import * as algokit from '@algorandfoundation/algokit-utils'
+import { AppReference } from '@algorandfoundation/algokit-utils/types/app'
+import { TransactionSigner } from 'algosdk'
+import * as appSpec from '../../../algorand/smart_contracts/artifacts/VotingRoundApp/application.json'
+import { encodeAnswerId, encodeAnswerIdBoxRef, encodeAnswerIdBoxRefs, encodeAnswerIds } from './question-encoding'
 
 export const algod = algokit.getAlgoClient({
   server: import.meta.env.VITE_ALGOD_NODE_CONFIG_SERVER,
   port: import.meta.env.VITE_ALGOD_NODE_CONFIG_PORT,
   token: import.meta.env.VITE_ALGOD_NODE_CONFIG_TOKEN,
-});
+})
 
 export const indexer = algokit.getAlgoIndexerClient({
   server: import.meta.env.VITE_INDEXER_SERVER,
   port: import.meta.env.VITE_INDEXER_PORT,
   token: import.meta.env.VITE_INDEXER_TOKEN,
-});
+})
 
 export const VotingRoundContract = (activeAddress: string, signer: TransactionSigner) => {
   const sender = {
     addr: activeAddress,
     signer: signer,
-  };
+  }
 
   const create = async (publicKey: Uint8Array, cid: string, start: number, end: number, quorum: number): Promise<AppReference> => {
     const appClient = algokit.getApplicationClient(
@@ -29,16 +29,16 @@ export const VotingRoundContract = (activeAddress: string, signer: TransactionSi
         id: 0,
         sender,
       },
-      algod
-    );
+      algod,
+    )
 
     const app = await appClient.create({
-      method: "create",
+      method: 'create',
       methodArgs: [publicKey, cid, start, end, quorum],
       deletable: false,
-    });
-    return app;
-  };
+    })
+    return app
+  }
 
   const bootstrap = async (app: AppReference, questionIds: string[]) => {
     const appClient = algokit.getApplicationClient(
@@ -47,14 +47,14 @@ export const VotingRoundContract = (activeAddress: string, signer: TransactionSi
         id: app.appId,
         sender,
       },
-      algod
-    );
+      algod,
+    )
 
     const questions = {
       unencoded: questionIds,
       encoded: encodeAnswerIds(questionIds),
       boxRefs: encodeAnswerIdBoxRefs(questionIds),
-    };
+    }
 
     const payTxn = (
       await algokit.transferAlgos(
@@ -64,31 +64,31 @@ export const VotingRoundContract = (activeAddress: string, signer: TransactionSi
           amount: algokit.microAlgos(100_000 + questions.unencoded.length * (400 * /* key size */ (18 + /* value size */ 8) + 2500)),
           skipSending: true,
         },
-        algod
+        algod,
       )
-    ).transaction;
+    ).transaction
     const callTxn = (
       await appClient.call({
-        method: "bootstrap",
+        method: 'bootstrap',
         methodArgs: {
           args: [/*payTxn, */ questions.encoded],
           boxes: questions.boxRefs,
         },
         sendParams: { skipSending: true },
       })
-    ).transaction;
+    ).transaction
     try {
       await algokit.sendGroupOfTransactions(
         {
           transactions: [payTxn, callTxn],
           signer: sender,
         },
-        algod
-      );
+        algod,
+      )
     } catch (e) {
-      throw appClient.exposeLogicError(e as Error);
+      throw appClient.exposeLogicError(e as Error)
     }
-  };
+  }
 
   const castVote = async (signature: string, selectedOption: string, appId: number) => {
     const client = algokit.getApplicationClient(
@@ -96,14 +96,14 @@ export const VotingRoundContract = (activeAddress: string, signer: TransactionSi
         app: JSON.stringify(appSpec),
         id: appId,
       },
-      algod
-    );
+      algod,
+    )
 
-    const signatureByArray = Buffer.from(signature, "base64");
-    const voteFee = algokit.microAlgos(1_000 + 3 /* opup - 700 x 3 to get 2000 */ * 1_000);
+    const signatureByArray = Buffer.from(signature, 'base64')
+    const voteFee = algokit.microAlgos(1_000 + 3 /* opup - 700 x 3 to get 2000 */ * 1_000)
 
     const transaction = await client.call({
-      method: "vote",
+      method: 'vote',
       methodArgs: {
         args: [signatureByArray, encodeAnswerId(selectedOption)],
         boxes: [encodeAnswerIdBoxRef(selectedOption, await client.getAppReference())],
@@ -113,14 +113,14 @@ export const VotingRoundContract = (activeAddress: string, signer: TransactionSi
         signer: signer,
       },
       sendParams: { fee: voteFee },
-    });
+    })
 
-    return transaction;
-  };
+    return transaction
+  }
 
   return {
     create,
     bootstrap,
     castVote,
-  };
-};
+  }
+}
--- src/dapp/src/vite-env.d.ts
+++ src/dapp/src/vite-env.d.ts
@@ -1,20 +1,13 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-<<<<<<< HEAD
-  readonly VITE_ALGOD_NODE_CONFIG_TOKEN: string;
-  readonly VITE_ALGOD_NODE_CONFIG_SERVER: string;
-  readonly VITE_ALGOD_NODE_CONFIG_PORT: string;
-  readonly VITE_INDEXER_SERVER: string;
-  readonly VITE_INDEXER_PORT: string;
-  readonly VITE_INDEXER_TOKEN: string;
-  readonly VITE_ALGOD_NETWORK: string;
-=======
   readonly VITE_ALGOD_NODE_CONFIG_TOKEN: string
   readonly VITE_ALGOD_NODE_CONFIG_SERVER: string
-  readonly VITE_ALGOD_NOTE_CONFIG_PORT: string
+  readonly VITE_ALGOD_NODE_CONFIG_PORT: string
+  readonly VITE_INDEXER_SERVER: string
+  readonly VITE_INDEXER_PORT: string
+  readonly VITE_INDEXER_TOKEN: string
   readonly VITE_ALGOD_NETWORK: string
->>>>>>> main
 }
 
 interface ImportMeta {
