2f59614f9f42d27dd54d00466895d868d932ffcf
Rob Moore <rob.moore@makerx.com.au>
2023-04-07 14:37:25 +0800

    Added better indication of result if someone voted

--- src/dapp/src/features/vote/index.tsx
+++ src/dapp/src/features/vote/index.tsx
@@ -102,7 +102,7 @@ function Vote() {
               <Skeleton variant="rectangular" className="h-10" />
             </Stack>
           ) : (
-            <WalletVoteStatus round={data} allowedToVote={allowedToVote} />
+            <WalletVoteStatus round={data} allowedToVote={allowedToVote} myVote={voteResult} />
           )}
 
           {!loading && voteEnded && (
@@ -128,22 +128,24 @@ function Vote() {
 
               <Typography>{question.description}</Typography>
 
-              <div className="mt-4">
-                {loadingVote ? (
-                  <SkeletonArray className="max-w-xs" count={1} />
-                ) : (
-                  <>
-                    {canVote || !voteStarted ? (
-                      <VoteSubmission round={data} existingAnswer={voteResult} handleSubmitVote={handleSubmitVote} />
-                    ) : null}
-                  </>
-                )}
-              </div>
+              {!voteResult && (
+                <div className="mt-4">
+                  {loadingVote ? (
+                    <SkeletonArray className="max-w-xs" count={1} />
+                  ) : (
+                    <>
+                      {canVote || !voteStarted ? (
+                        <VoteSubmission round={data} existingAnswer={voteResult} handleSubmitVote={handleSubmitVote} />
+                      ) : null}
+                    </>
+                  )}
+                </div>
+              )}
               <div className="mt-4">
                 {loadingResults ? (
                   <SkeletonArray className="max-w-xs" count={4} />
                 ) : (
-                  votingRoundResults && <VoteResults question={question} votingRoundResults={votingRoundResults} />
+                  votingRoundResults && <VoteResults question={question} votingRoundResults={votingRoundResults} myVote={voteResult} />
                 )}
               </div>
               {error && (
