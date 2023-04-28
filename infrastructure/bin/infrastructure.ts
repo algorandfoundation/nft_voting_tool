647faaea39602ebdf0784c4671274e3e10b19c1b
Vatsalya Goel <vatsalyagoel@gmail.com>
2023-04-28 06:01:10 +0400

    Move to using props rather than hard coding in stack template

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+import { Duration } from 'aws-cdk-lib'
+import { HeadersFrameOption, HeadersReferrerPolicy, ResponseHeadersPolicyProps } from 'aws-cdk-lib/aws-cloudfront'
 import * as path from 'path'
 import { ApiStack } from '../lib/api-stack'
 import { CDKDeployer } from '../lib/cdk-deployer'
@@ -34,10 +36,54 @@ const dns = appDomainName
     )
   : undefined
 
+const responseHeaders = {
+  securityHeadersBehavior: {
+    contentTypeOptions: {
+      override: false,
+    },
+    strictTransportSecurity: {
+      accessControlMaxAge: Duration.days(365),
+      override: false,
+      includeSubdomains: true,
+      preload: true,
+    },
+    xssProtection: {
+      override: false,
+      modeBlock: true,
+      protection: true,
+    },
+    referrerPolicy: {
+      override: false,
+      referrerPolicy: HeadersReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN,
+    },
+    frameOptions: {
+      override: false,
+      frameOption: HeadersFrameOption.DENY,
+    },
+  },
+  customHeadersBehavior: {
+    customHeaders: [
+      {
+        header: 'Content-Security-Policy-Report-Only',
+        override: false,
+        value:
+          "default-src 'self'; script-src 'self' 'sha256-Wgxes77BIEtCJ/4JOr9+KiFe2QGbbqLEEBv2+rbrnbk='; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com; connect-src *; media-src 'self'; object-src 'none'; frame-src 'self'; worker-src 'none'; upgrade-insecure-requests; base-uri 'self'; manifest-src 'self'",
+      },
+      {
+        header: 'Permissions-Policy',
+        override: false,
+        value: 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()',
+      },
+    ],
+  },
+  removeHeaders: ['server'],
+} as ResponseHeadersPolicyProps
+
 const app = deployer.deploy(StaticWebsiteStack, 'web', {
   websiteFolder: process.env.WEBSITE_BUILD_PATH ?? path.join(__dirname, '..', '..', 'src', 'dapp', 'dist'),
   websiteNpmBuildCommand: 'build-dapp',
   customDomain: dns?.getDefaultCustomDomainProps(deployer.defaultRegion, appDomainName),
+  responseHeaders: responseHeaders,
 })
 
 const api = deployer.deploy(ApiStack, 'api', {
