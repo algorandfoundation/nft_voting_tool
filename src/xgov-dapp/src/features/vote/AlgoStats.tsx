47e32b01c0f9aa8b83054ef7182a275339a89583
Richard Watts-Seale <rwattsseale@gmail.com>
2023-08-04 09:05:59 -0500

    Dont show redundant data on results screen

--- src/xgov-dapp/src/features/vote/AlgoStats.tsx
+++ src/xgov-dapp/src/features/vote/AlgoStats.tsx
@@ -36,7 +36,7 @@ const AlgoStats: React.FC<AlgoStatsProps> = ({ votingRoundResults, votingRoundMe
                   ? `${totalAwarded.toLocaleString()} out of ${totalAsked.toLocaleString()} ALGO awarded`
                   : `${totalAsked.toLocaleString()} total ALGO asked`}
               </Typography>
-              <Typography>{`${totalAwarded.toLocaleString()} ALGO approved`}</Typography>
+              <Typography>{!hasVoteClosed ? `${totalAwarded.toLocaleString()} ALGO approved` : ''}&nbsp;</Typography>
             </>
           ) : null}
         </div>
