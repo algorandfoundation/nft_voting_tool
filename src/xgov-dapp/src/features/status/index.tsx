28427e286d233b927a31b460373d63c5aa8c1a7a
Ori Shem-Tov <orishemtov@gmail.com>
2023-10-11 15:11:50 +0300

    change total xgov deposit to be the sum of all current rewards instead of original rewards

--- src/xgov-dapp/src/features/status/index.tsx
+++ src/xgov-dapp/src/features/status/index.tsx
@@ -156,10 +156,10 @@ function Status() {
           <Box className="bg-white flex rounded-xl px-4 py-6">
             <div className="w-full">
               <Typography className="mb-3">
-                <strong>Your xGov deposit</strong>
+                <strong>Your xGov Current Balance</strong>
               </Typography>
               <Typography variant="h3">
-                {govenorData.reduce((sum, item) => sum + parseInt(item.original_reward), 0).toLocaleString()} µA
+                {govenorData.reduce((sum, item) => sum + parseInt(item.current_reward), 0).toLocaleString()} µA
               </Typography>
             </div>
           </Box>
